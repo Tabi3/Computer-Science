0ff5d99a21e960524a9b177e78b5c09b3671b219
Tabi3 <viper0998975@gmail.com>
2022-11-22 18:08:34 +0800

    Massive Overhaul

--- make_venv.py
+++ make_venv.py
@@ -7,14 +7,18 @@ if __name__ == "__main__":
     main()
 
 """
+
 import os
 
-command: str = "python -m venv " 
+command: str = "python -m venv "
 fp     : str = input("Name : ")
- 
-os.system(f'{command}"{fp}"'  )
-os.system(f'mkdir "{fp} main"')
-with open(f"./{fp} main/main.py", 'w') as f:
+
+os.system(f'mkdir "{fp}"')
+os.chdir(f"{fp}")
+
+os.system(f'{command} "{fp} Dependancies"'  )
+os.system('mkdir main')
+with open("./main/main.py", 'w') as f:
     f.write(__doc__)
 
 print("Finished creating virtual ENV...")
